f66844e6feeaf011ae93dfaf35920558f50f6f43
StefanZur <stefanzurmangoncalves@gmail.com>
2023-08-16 22:06:06 -0300

    Correção no banco

--- rdocs/stefan.R
+++ rdocs/stefan.R
@@ -22,3 +22,38 @@ source("rdocs/source/packages.R")
 # de teste depreciados, ou ao menos deixando como comentário. Dê preferência
 # as funções dos pacotes contidos no Tidyverse para realizar suas análises.
 # ---------------------------------------------------------------------------- #
+
+Banco <- readRDS("banco/df.rds")
+
+Supra <- Banco %>% filter(tipo == "supra")
+
+#Latência
+#Anova
+SupraLat <- Supra %>% filter(amp_lat == "LAT")
+AnovaLatSup <- aov(SupraLat$valores ~ as.factor(SupraLat$musculo))
+summary(AnovaLatSup)
+par(mfrow=c(1,3))
+
+#Pressupostos (Usar resíduos Studentizados)
+Res <- rstudent(AnovaLatSup)
+yfit <- fitted(AnovaLatSup)
+ResLatSup <- data.frame(Res=Res,yfit=yfit)
+#Normalidade
+ggplot(ResLatSup) +
+  aes(x = yfit, y = Res) +
+  geom_point(colour = "black", size = 3) +
+  labs(
+    x = "valores ajustados da resposta",
+    y = "resíduos Studentizados"
+  ) +
+  theme_estat()
+#ggsave("disp_ResValaj.pdf", width = 158, height = 93, units = "mm")
+shapiro.test(ResLatSup)
+qqnorm(ResLatSup)
+qqline(ResLatSup)
+
+#Independencia
+plot(ResLatSup)
+
+#Homocedasticidade
+plot(ResLatSup, AnovaLatSup$fitted.values)
